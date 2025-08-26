397474370f374dc94942c02925783155ab3ab3bb
dieunt <azure3132005@gmail.com>
2025-08-26 10:29:14 +0700

    no message

--- lib_bk/src/utils/web_utils_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:typed_data';
-
-/// Stub function for non-web platforms
-void downloadFile(Uint8List bytes, String filename) {
-  throw UnsupportedError('downloadFile is only supported on web platform');
-}
