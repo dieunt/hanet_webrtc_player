a9755a9615df0d3ede39d792b112c5f4bdc1a989
dieunt <azure3132005@gmail.com>
2025-08-26 16:13:42 +0700

    no message

--- lib/src/utils/BleManager.dart
+++ lib/src/utils/BleManager.dart
@@ -1,77 +1,77 @@
-import 'dart:async';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'dart:developer';
-import 'dart:core';
+// import 'dart:async';
+// import 'package:flutter_blue/flutter_blue.dart';
+// import 'dart:developer';
+// import 'dart:core';
 
-typedef ScanResultHandler = void Function(List<ScanResult>? datas);
-typedef CharacteristicHandler = void Function(List<int>? datas);
+// typedef ScanResultHandler = void Function(List<ScanResult>? datas);
+// typedef CharacteristicHandler = void Function(List<int>? datas);
 
-class BleManager {
-  FlutterBlue? _flutterBlue;
-  factory BleManager() => _getInstance();
-  static BleManager get instance => _getInstance();
-  static  BleManager? _instance;
-   BleManager._internal() {
-    _flutterBlue = FlutterBlue.instance;
-  }
-  static BleManager _getInstance() {
-    if (_instance == null) {
-      _instance = BleManager._internal();
-    }
-    return _instance!;
-  }
+// class BleManager {
+//   FlutterBlue? _flutterBlue;
+//   factory BleManager() => _getInstance();
+//   static BleManager get instance => _getInstance();
+//   static  BleManager? _instance;
+//    BleManager._internal() {
+//     _flutterBlue = FlutterBlue.instance;
+//   }
+//   static BleManager _getInstance() {
+//     if (_instance == null) {
+//       _instance = BleManager._internal();
+//     }
+//     return _instance!;
+//   }
 
-  startScan(ScanResultHandler dataHandler, {int timeout = 60}) {
-    log("_scan");
-    // _flutterBlue?.startScan(timeout: Duration(seconds: timeout));
+//   startScan(ScanResultHandler dataHandler, {int timeout = 60}) {
+//     log("_scan");
+//     // _flutterBlue?.startScan(timeout: Duration(seconds: timeout));
 
-    _flutterBlue?.startScan(timeout: Duration(seconds: timeout),withServices: [Guid("0000fff0-0000-1000-XXXX-XXXXXXXXXXX")]);
-    _flutterBlue?.scanResults.listen(dataHandler);
-  }
+//     _flutterBlue?.startScan(timeout: Duration(seconds: timeout),withServices: [Guid("0000fff0-0000-1000-XXXX-XXXXXXXXXXX")]);
+//     _flutterBlue?.scanResults.listen(dataHandler);
+//   }
 
-  stopScan() {
-    log("_stopScan");
-    _flutterBlue?.stopScan();
-  }
+//   stopScan() {
+//     log("_stopScan");
+//     _flutterBlue?.stopScan();
+//   }
 
-  Future<void> connectDevice(BluetoothDevice device) async {
-    log("_connect");
-    await device.connect(autoConnect: false);
-  }
+//   Future<void> connectDevice(BluetoothDevice device) async {
+//     log("_connect");
+//     await device.connect(autoConnect: false);
+//   }
 
-  Future<List<BluetoothService>> deviceToDiscoverServices(
-      BluetoothDevice device) async {
-    return await device.discoverServices();
-  }
+//   Future<List<BluetoothService>> deviceToDiscoverServices(
+//       BluetoothDevice device) async {
+//     return await device.discoverServices();
+//   }
 
-  Future<bool> characteristicToSetNotifyValue(
-      BluetoothCharacteristic characteristic) async {
-    return await characteristic.setNotifyValue(true);
-  }
+//   Future<bool> characteristicToSetNotifyValue(
+//       BluetoothCharacteristic characteristic) async {
+//     return await characteristic.setNotifyValue(true);
+//   }
 
-  listenCharacteristicValue(BluetoothCharacteristic characteristic,
-      CharacteristicHandler characteristicHandler) {
-    characteristic.value.listen(characteristicHandler);
-  }
+//   listenCharacteristicValue(BluetoothCharacteristic characteristic,
+//       CharacteristicHandler characteristicHandler) {
+//     characteristic.value.listen(characteristicHandler);
+//   }
 
-  Future<Null> characteristicToWriteValue(
-      BluetoothCharacteristic characteristic, List<int> list,{bool withoutResponse = false}) async {
-    log("_write  withoutResponse = $withoutResponse",);
-    return await characteristic.write(list,withoutResponse: withoutResponse);
-  }
+//   Future<Null> characteristicToWriteValue(
+//       BluetoothCharacteristic characteristic, List<int> list,{bool withoutResponse = false}) async {
+//     log("_write  withoutResponse = $withoutResponse",);
+//     return await characteristic.write(list,withoutResponse: withoutResponse);
+//   }
 
-  Future<List<int>> characteristicToReadValue(
-      BluetoothCharacteristic characteristic) async {
-    log("_read");
-    return await characteristic.read();
-  }
+//   Future<List<int>> characteristicToReadValue(
+//       BluetoothCharacteristic characteristic) async {
+//     log("_read");
+//     return await characteristic.read();
+//   }
 
-  Future<List<BluetoothDevice>>? getConnectedDevices() {
-    return _flutterBlue?.connectedDevices;
-  }
+//   Future<List<BluetoothDevice>>? getConnectedDevices() {
+//     return _flutterBlue?.connectedDevices;
+//   }
 
-  Future<dynamic> disconnectDevice(BluetoothDevice device) async {
-    log("_disconnect");
-    return await device.disconnect();
-  }
-}
+//   Future<dynamic> disconnectDevice(BluetoothDevice device) async {
+//     log("_disconnect");
+//     return await device.disconnect();
+//   }
+// }
